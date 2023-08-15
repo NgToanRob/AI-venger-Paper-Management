aa8f7421c33dc586fa8f2f0742102c5f8d5a8d98
NgToanRob <nguyentoan10601@gmail.com>
2023-08-15 16:03:42 +0700

    resolve conflict

--- PaperManagement/settings.py
+++ PaperManagement/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-sp#c!c$_%&%)7!s3i^-jbzl7q#@__&)pgv7g7#yc$+xxz7r3&h"
+SECRET_KEY = 'django-insecure-sp#c!c$_%&%)7!s3i^-jbzl7q#@__&)pgv7g7#yc$+xxz7r3&h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -34,55 +34,54 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "Paper",
-    "User",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'Paper',
+    'User',
+    'SearchEngine'
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = "PaperManagement.urls"
+ROOT_URLCONF = 'PaperManagement.urls'
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = "PaperManagement.wsgi.application"
+WSGI_APPLICATION = 'PaperManagement.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -103,37 +102,26 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-# Add this line to enable authentication using email instead of the default username
-AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.AllowAllUsersModelBackend']\
-
-# The URL to redirect to after successful login
-LOGIN_REDIRECT_URL = '/'
-
-# The URL to redirect to when login is required
-LOGIN_URL = 'auth/login/'
-
-AUTH_USER_MODEL = 'User.CustomUser' 
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = "UTC"
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -143,15 +131,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # Add your frontend's origin here
-]
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ SearchEngine/views.py
@@ -0,0 +1,64 @@
+from django.http import JsonResponse
+from arxiv import Search, SortCriterion, SortOrder
+from django.shortcuts import render
+from .models import SearchHistory
+
+def search_arxiv(request):
+    query_string = request.GET.get('query')
+    results = []
+    if query_string:
+        SearchHistory.objects.create(query=query_string)
+    if query_string:
+        # Search arXiv and get the results
+        search_results = Search(query=query_string, max_results=10, sort_by=SortCriterion.Relevance, sort_order=SortOrder.Descending)
+
+        # Extract relevant data and build a list of dictionaries
+        for result in search_results.results():
+            # Extract author names from Author objects
+            author_names = [author.name for author in result.authors]
+            
+            result_dict = {
+                'title': result.title,
+                'authors': ', '.join(author_names),
+                'abstract': result.summary,
+                'arxiv_id': result.entry_id,
+                'published_date': result.published,
+                'url': result.pdf_url
+            }
+            results.append(result_dict)
+            
+    return JsonResponse(results, safe=False)
+
+from django.db.models import Count
+from datetime import datetime
+
+def recommended_papers(request):
+    # Get the top 5 most searched queries
+    top_queries = SearchHistory.objects.values('query').annotate(query_count=Count('query')).order_by('-query_count')[:7]
+
+    recommended_results = []
+    processed_topics = set()
+    papers = set()
+    for entry in top_queries:
+        query = entry['query']
+        if query in processed_topics:
+            continue
+
+        # Search arXiv based on the query and get relevant information
+        search_results = Search(query=query, max_results=1, sort_by=SortCriterion.LastUpdatedDate, sort_order=SortOrder.Descending)
+        search_results_list = list(search_results.results())
+        if search_results_list:
+            paper = search_results_list[0]
+            if paper.title not in papers:
+                author_names = [author.name for author in paper.authors]
+                recommended_results.append({
+                    'title': paper.title,
+                    'authors': ', '.join(author_names),
+                    'abstract': paper.summary,
+                    'published_date': paper.published,
+                    'url': paper.pdf_url,
+                    'related': query
+                })
+            papers.add(paper.title)
+        processed_topics.add(query)
+    return JsonResponse(recommended_results, safe=False)
